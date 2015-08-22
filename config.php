99c0dd91ea2d0363f3efb002ed01697a4e36c69a
Jerod Lycett <arenlor@arenlor.com>
2015-08-22 08:18:57 -0400

    Created registration page
    Created the registration page, needed a new variable.
    
    Signed-off-by: Jerod Lycett <arenlor@arenlor.com>

--- config.php
+++ config.php
@@ -3,4 +3,5 @@ $salt = "";
 $iterations = 1001;
 /* Set to a custom salt, and if you're really paranoid a custom iteration */
 $orgName = ""; // Your organization name.
+$wapath = ""; // The path with domain to your server. https://arenlor.com/workaccounter/ is mine
 ?>
---
+++ register.php
@@ -0,0 +1,41 @@
+<?php session_name("workaccounter");
+session_start();
+if(isset($_SESSION["userid"]) && !empty($_SESSION["userid"])) {
+    require_once("config.php");
+    header("Location: " . $wapath . "login.php");
+    exit();
+}
+elseif((isset($_POST["username"]) && !empty($_POST["username"])) && (isset($_POST["password"]) && !empty($_POST["password"]))) {
+    $password = $_POST["password"];
+    require_once("config.php");
+    for($i=0;$i<$iterations;$i++) {
+        $password = hash($hash, $password . $salt);
+    }
+    $username = pg_escape_string($_POST["username"]);
+    $apikey = hash($hash, $salt . $password . $username . time());
+    require_once("conn.php");
+    $insertquery = "INSERT INTO users VALUES(\"" . $username . "\", \"" . $password . "\", \"" . $apikey . "\", 0)";
+    $res = pg_query($pgconn, $inserquery);
+    if(!$res) {
+        echo "Error occured in result, maybe non-unique username. Dieing.\n";
+        break();
+    }
+    $_SESSION["userid"] = $apikey;
+    header("Location: " . $wapath . "index.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title><?php echo($orgName); ?> - Register</title>
+    </head>
+    <body>
+        <form action="register.php" method="POST">
+            <label for="username">Username (must be unqiue): </label><input type="text" id="username" name="username"><br>
+            <label for="password">Password: </label><input type="password" id="password" name="password">
+            <input type="submit" value="Register">
+        </form>
+    </body>
+</html>
