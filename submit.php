92ffc3f4dda6eb9943e453ddd1f9752be9d237b6
Jerod Lycett <arenlor@arenlor.com>
2015-08-22 10:42:35 -0400

    Fix typo in submit
    Signed-off-by: Jerod Lycett <arenlor@arenlor.com>

--- submit.php
+++ submit.php
@@ -2,7 +2,7 @@
     $count = pg_escape_string($_GET["count"]);
     $apikey = pg_escape_string($_GET["apikey"]);
     require_once("conn.php");
-    $updatequery = "UPDATE users SET count=" . $count . " WHERE apikey=\"" . $apikey . "\"";
+    $updatequery = "UPDATE users SET count=" . $count . " WHERE apikey='" . $apikey . "'";
     pg_query($pgconn, $updatequery);
 }
 ?>
