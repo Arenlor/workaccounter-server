87536cf138e309151a25da4848fa58fc5b73d7ad
Jerod Lycett <arenlor@arenlor.com>
2015-08-22 08:27:51 -0700

    Fixed typo
    I missed a small typo, maybe need to stop using nano ^_^'
    
    Signed-off-by: Jerod Lycett <arenlor@arenlor.com>

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en-US">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title><?php require_once("config.php"); echo($orgName); ?> - <?php echo($pagetitle);</title>
+<title><?php require_once("config.php"); echo($orgName); ?> - <?php echo($pagetitle); ?></title>
 </head>
 <body>
 <div><a href="index.php">Home</a> | <a href="stats.php">Stats</a> | <a href="register.php">Register</a> or <a href="login.php">Login</a></div>
