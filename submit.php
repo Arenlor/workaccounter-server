e23ad12a4581a9a3962083847c43f80a922e621a
Jerod Lycett <arenlor@arenlor.com>
2015-08-22 08:46:31 -0700

    Corrected 0 reset
    Corrected the code to allow count=0
    
    Signed-off-by: Jerod Lycett <arenlor@arenlor.com>

--- submit.php
+++ submit.php
@@ -1,4 +1,4 @@
-<?php if((isset($_GET["apikey"]) && !empty($_GET["apikey"])) && (isset($_GET["count"]) && !empty($_GET["count"]))) {
+<?php if((isset($_GET["apikey"]) && !empty($_GET["apikey"])) && (isset($_GET["count"]) && (!empty($_GET["count"]) || $_GET["count"] == 0))) {
     $count = pg_escape_string($_GET["count"]);
     $apikey = pg_escape_string($_GET["apikey"]);
     require_once("conn.php");
